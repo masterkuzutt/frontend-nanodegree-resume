65af17cb92b2b81d2e541884366383e16e9a8e54
taka <masterkuzutt+github@gmail.com>
2016-08-01 21:09:51 +0900

    add objects [bio,education, work, project]

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,67 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio{
+      "name": "Takahiro Sakai",
+      "role": "Web Developer",
+      "contacts" : {
+        "mobile": "xxx-xxxx-xxxx",
+        "email": "kuzutt@example.com",
+        "github": "masterkuzutt",
+        "twitter": "masterkuzutt",
+        "location": "Tokyo"
+      },
+      "welcomeMessage": "hoge huga hoge fuga hoge",
+      "skills": [
+        "eat 1 dish a date","wight training"
+      ],
+      "bioPic": "images/my.jpg"
+    },
+    education : {
+      "schools": [
+        {
+          "name": "Toyo university",
+          "city": "Tokyo JP",
+          "degree": "Blchelor",
+          "majors" : "Economics",
+          "dates" 2005,
+          "url" : "http://example.com"
+        }
+      ],
+      "onlineCourses": [
+        {
+          "title": "Front-End Web Developper Nanodegree",
+          "school": "Udacity",
+          "dates" : 2016,
+          "url" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+        }
+      ]
+    },
+    work = {
+      "jobs" : [
+        {
+          "position": "Leading Private",
+          "employer": "Japan Self Defence Forces",
+          "locaiton": "tokyo JP",
+          "years": "2005-2007",
+          "discripion": ""
+        },
+        {
+          "positon": "Engineer",
+          "employer": "M.O.C",
+          "locaiton": "tokyo JP",
+          "years": "2007-2016",
+          "discripion":"hugahoga"
+        }
+      ]
+    },
+    project = {
+      "projects": [
+          {
+            "title": "Sample Project 1",
+            "dates": "2015",
+            "discription":"wa-wa-wa-wa-wa",
+            "images":[
+              "dummy url1",
+              "dummy url2"
+            ]
+          }
+        ]
+    },
